184ffc7aaf882f9ee19ec71d6f876fc273be7145
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-02-28 00:37:52 +0100

    add jwt middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,8 +54,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- routes/api.php
+++ routes/api.php
@@ -25,41 +25,46 @@ use App\Http\Controllers\ChefShipperController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::post("/reviews",[ReviewsController::class,"post"]);
-Route::post("/updatePicture",[RegistrationController::class,"updatePicture"]);
-Route::get("/userdata ",[UserdataController::class,"getUserdata"]);
-Route::post("/signup",[RegistrationController::class,"signup"]);
+
+
 Route::post("/login ",[AuthController::class,"login"]);
-Route::post("/addProduct ",[ProductsController::class,"saveProduct"]);
-Route::get("/retrieveProducts ",[ProductsController::class,"retrieveProducts"]);
-Route::get("/retrieveProductDetails ",[ProductsController::class,"retrieveProductDetails"]);
-Route::post("/saveFeedbacks ",[NotificationsController::class,"saveFeedbacks"]);
-Route::get("/retrieveFeedbacks ",[NotificationsController::class,"retrieveFeedbacks"]);
-Route::get("/retrieveRestaurents ",[RestaurentsController::class,"retrieveRestaurents"]);
-Route::get("/retrieveRestaurents ",[RestaurentsController::class,"retrieveRestaurents"]);
-Route::post("/sendRating ",[NotificationsController::class,"sendRating"]);
-Route::post("/updateProfile ",[UserdataController::class,"updateProfile"]);
-Route::post("/sendMsg ",[ChatController::class,"sendMsg"]);
-Route::get("/retrieveMsgs ",[ChatController::class,"retrieveMsgs"]);
-Route::get("/retrieveMessagedUsers ",[ChatController::class,"retrieveMessagedUsers"]);
-Route::get("/searchForRestaurents ",[RestaurentsController::class,"searchForRestaurents"]);
-Route::post("/saveOrders ",[NotificationsController::class,"saveOrders"]);
-Route::get("/retrieveOrders ",[NotificationsController::class,"retrieveOrders"]);
-Route::get("/deleteOrders ",[NotificationsController::class,"deleteOrders"]);
-Route::get("/retrieveShipperNotifications ",[NotificationsController::class,"retrieveShipperNotifications"]);
-Route::get("/retrieveNotifications ",[NotificationsController::class,"retrieveNotifications"]); 
-Route::post("/updateNotifStatus ",[NotificationsController::class,"updateNotifStatus"]); 
-Route::post("/saveShippers ",[ChefShipperController::class,"saveShippers"]);
-Route::get("/retrieveShippers ",[ChefShipperController::class,"retrieveShippers"]); 
-Route::get("/deleteProducts ",[ProductsController::class,"deleteProducts"]); 
-Route::post("/updateRequestStatus ",[ChefShipperController::class,"updateRequestStatus"]);
-Route::post("/saveProductFeedback ",[NotificationsController::class,"saveProductFeedback"]);
-Route::get("/retrieveProductsFeedbacks ",[NotificationsController::class,"retrieveProductsFeedbacks"]);
-Route::post("/sendProductRating ",[NotificationsController::class,"sendProductRating"]);
-Route::get("/checkRating ",[NotificationsController::class,"checkRating"]);
+Route::post("/signup",[RegistrationController::class,"signup"]);
+
+Route::middleware(['jwt.verify'])->group(function(){
+
+ 
+    Route::post("/reviews",[ReviewsController::class,"post"]);
+    Route::post("/updatePicture",[RegistrationController::class,"updatePicture"]);
+    Route::get("/userdata ",[UserdataController::class,"getUserdata"]);
+    Route::post("/addProduct ",[ProductsController::class,"saveProduct"]);
+    Route::get("/retrieveProducts ",[ProductsController::class,"retrieveProducts"]);
+    Route::get("/retrieveProductDetails ",[ProductsController::class,"retrieveProductDetails"]);
+    Route::post("/saveFeedbacks ",[NotificationsController::class,"saveFeedbacks"]);
+    Route::get("/retrieveFeedbacks ",[NotificationsController::class,"retrieveFeedbacks"]);
+    Route::get("/retrieveRestaurents ",[RestaurentsController::class,"retrieveRestaurents"]);
+    Route::post("/sendRating ",[NotificationsController::class,"sendRating"]);
+    Route::post("/updateProfile ",[UserdataController::class,"updateProfile"]);
+    Route::post("/sendMsg ",[ChatController::class,"sendMsg"]);
+    Route::get("/retrieveMsgs ",[ChatController::class,"retrieveMsgs"]);
+    Route::get("/retrieveMessagedUsers ",[ChatController::class,"retrieveMessagedUsers"]);
+    Route::get("/searchForRestaurents ",[RestaurentsController::class,"searchForRestaurents"]);
+    Route::post("/saveOrders ",[NotificationsController::class,"saveOrders"]);
+    Route::get("/retrieveOrders ",[NotificationsController::class,"retrieveOrders"]);
+    Route::get("/deleteOrders ",[NotificationsController::class,"deleteOrders"]);
+    Route::get("/retrieveShipperNotifications ",[NotificationsController::class,"retrieveShipperNotifications"]);
+    Route::get("/retrieveNotifications ",[NotificationsController::class,"retrieveNotifications"]); 
+    Route::post("/updateNotifStatus ",[NotificationsController::class,"updateNotifStatus"]); 
+    Route::post("/saveShippers ",[ChefShipperController::class,"saveShippers"]);
+    Route::get("/retrieveShippers ",[ChefShipperController::class,"retrieveShippers"]); 
+    Route::get("/deleteProducts ",[ProductsController::class,"deleteProducts"]); 
+    Route::post("/updateRequestStatus ",[ChefShipperController::class,"updateRequestStatus"]);
+    Route::post("/saveProductFeedback ",[NotificationsController::class,"saveProductFeedback"]);
+    Route::get("/retrieveProductsFeedbacks ",[NotificationsController::class,"retrieveProductsFeedbacks"]);
+    Route::post("/sendProductRating ",[NotificationsController::class,"sendProductRating"]);
+    Route::get("/checkRating ",[NotificationsController::class,"checkRating"]);
+
+});
+
 
 Route::group(['middleware'=>'api'],function(){
 
